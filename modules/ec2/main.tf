f7ef74be3a3fc393a9e062261b8d397a391b0fc4
rajeshsunmugam <rajeshsunmugam04@gmail.com>
2026-01-06 17:38:52 +0530

    Add files via upload

---
+++ modules/ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "ec2" {
+  ami           = var.aws_amiid
+  instance_type = var.aws_type
+  count         = var.aws_count
+
+  tags = {
+    Name = "${var.env}-ec2-${count.index}"
+    Env= var.env
+  }
+}
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "env" {
+    type = string
+    description = "which env has to be in deploy"
+}
+
+variable "aws_type" {
+    type = string
+    description = "which env has to be in deploy"
+}
+
+variable "aws_count" {
+    type = string
+    description = "which env has to be in deploy"
+}
+
+variable "aws_amiid" {
+    type = string
+    description = "which env has to be in deploy"
+}

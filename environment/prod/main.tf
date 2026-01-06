8b1d1d63afc0aa442532be1a42edfd6923c48109
rajeshsunmugam <rajeshsunmugam04@gmail.com>
2026-01-06 17:56:12 +0530

    Update main.tf

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -1,7 +1,8 @@
 module "ec2" {
-    source    = var.source 
+    source    = "../../modules/ec2" 
     aws_amiid = var.aws_amiid
     aws_type  = var.aws_type
     aws_count = var.aws_count
     env       = var.env
+
 }

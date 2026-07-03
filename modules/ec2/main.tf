176f58581641f9e348662e01d3f98c297d3c5134
rajeshsunmugam <rajeshsunmugam04@gmail.com>
2026-07-03 18:02:37 +0530

    Modify EC2 instance resource count variable
    Updated the EC2 instance resource configuration to use 'aws_count' variable.

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 /* resource "aws_instance" "ec2" {
   ami           = var.aws_amiid
   instance_type = var.aws_type
-  count         = var.aws_count
+  count         = var.aws_couple
 
   tags = {
     Name = "${var.env}-ec2-${count.index}"
